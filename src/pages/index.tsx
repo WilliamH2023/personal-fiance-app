953d6ea0bd42d3c23d852dea878b6cdc535c1ba6
William Huang <whuang1403@gmail.com>
2023-07-06 15:56:22 -0400

    add auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,10 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
-import { SignUpButton, SignInButton } from "@clerk/nextjs";
+import { SignUpButton, SignInButton, useUser, SignOutButton } from "@clerk/nextjs";
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const user = useUser();
 
   return (
     <>
@@ -14,8 +15,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div> <SignUpButton /></div>
-        <div> <SignInButton/></div>
+        <div> 
+          {!user.isSignedIn && <SignInButton/>}
+          {!!user.isSignedIn && <SignOutButton/>}
+        </div>
+        
       </main>
     </>
   );
