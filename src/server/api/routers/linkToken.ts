547a80e82695e95ac2eee19b26b76cdba6ac4200
William Huang <whuang1403@gmail.com>
2023-08-08 16:17:21 -0400

    remove plaid server api

--- src/server/api/routers/linkToken.ts
+++ src/server/api/routers/linkToken.ts
@@ -18,28 +18,28 @@ const configuration = new Configuration({
 const client = new PlaidApi(configuration);
 
 export const linkTokenRouter = createTRPCRouter({
-    createLinkToken: publicProcedure.query(async () => {
-        const request = {
-            user: { client_user_id: process.env.PLAID_CLIENT_ID },
-            client_name: "Plaid's Tiny Quickstart",
-            language: "en",
-            products: process.env.PLAID_PRODUCTS,
-            country_codes: process.env.PLAID_COUNTRY_CODES,
-            redirect_uri: "localhost:3000",
-        };
-        const tokenResponse = await client.linkTokenCreate(request);
+    // createLinkToken: publicProcedure.query(async () => {
+    //     const request = {
+    //         user: { client_user_id: process.env.PLAID_CLIENT_ID },
+    //         client_name: "Plaid's Tiny Quickstart",
+    //         language: "en",
+    //         products: process.env.PLAID_PRODUCTS,
+    //         country_codes: process.env.PLAID_COUNTRY_CODES,
+    //         redirect_uri: "localhost:3000",
+    //     };
+    //     const tokenResponse = await client.linkTokenCreate(request);
 
-        return JSON.stringify(tokenResponse.data);
-    }),
+    //     return JSON.stringify(tokenResponse.data);
+    // }),
 
-    exchangePublicToken: publicProcedure.query(async (publicToken: string) => {
-        const exchangeResponse = await client.itemPublicTokenExchange({
-            public_token: publicToken,
-        });
+    // exchangePublicToken: publicProcedure.query(async (publicToken: string) => {
+    //     const exchangeResponse = await client.itemPublicTokenExchange({
+    //         public_token: publicToken,
+    //     });
 
-        const access_token = exchangeResponse.data.access_token;
-        return access_token;
-    }),
+    //     const access_token = exchangeResponse.data.access_token;
+    //     return access_token;
+    // }),
 
     getSecretMessage: protectedProcedure.query(() => {
         return "you can now see this secret message!";
